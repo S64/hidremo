61784b059a3b0714dcf2dcf9f13fac3ff7e07b84
Shuma Yoshioka <s64.stdio@gmail.com>
2019-01-17 22:18:58 +0900

    Define consts

--- esp32dev/src/main.cpp
+++ esp32dev/src/main.cpp
@@ -5,6 +5,14 @@
 
 #include "BLEHIDDevice.h"
 
+#define HIDREMO_DEVICE_NAME "MyBLEDevice"
+#define HIDREMO_WIFI_SSID "dummy"
+#define HIDREMO_WIFI_PASSPHRASE "dummy"
+
+#define HIDREMO_WS_ENDPOINT_HOST = "example.com"
+#define HIDREMO_WS_ENDPOINT_PORT = 80
+#define HIDREMO_WS_ENDPOINT_PATH = "/ws"
+
 void setup()
 {
     Serial.begin(115200);
