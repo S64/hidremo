5a271d8c6d5e0086f8f57c44244279705cde4df3
Shuma Yoshioka <s64.stdio@gmail.com>
2019-01-18 08:43:42 +0900

    Impl mySink, mySource

--- backend/app/services/MyKeyEventService.scala
+++ backend/app/services/MyKeyEventService.scala
@@ -1,8 +1,10 @@
 package services
 
 import akka.NotUsed
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.Materializer
+import akka.stream.scaladsl.{BroadcastHub, Keep, MergeHub, Sink, Source}
 import com.google.inject.ImplementedBy
+import javax.inject.Inject
 import play.api.libs.json.JsValue
 
 @ImplementedBy(classOf[MyKeyEventServiceImpl])
@@ -15,10 +17,15 @@ trait MyKeyEventService {
 
 }
 
-class MyKeyEventServiceImpl extends MyKeyEventService {
+class MyKeyEventServiceImpl @Inject()(
+  implicit mat: Materializer
+) extends MyKeyEventService {
 
-  override val mySink: Sink[JsValue, NotUsed] = ???
-  override val mySource: Source[JsValue, NotUsed] = ???
+  override val (mySink, mySource) = {
+    MergeHub.source[JsValue]
+      .toMat(BroadcastHub.sink[JsValue])(Keep.both)
+      .run()
+  }
 
   override def sendKeyEvent(usage: Int, keyCode: Int): Unit = ???
 
