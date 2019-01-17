670aaceb9f54987c5a3807134d7ede50b9737da9
Shuma Yoshioka <s64.stdio@gmail.com>
2019-01-17 21:41:57 +0900

    Stub main.cpp

---
+++ esp32dev/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <BLEDevice.h>
+
+#include "BLEHIDDevice.h"
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+
+}
