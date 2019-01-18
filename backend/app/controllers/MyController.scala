0e8cf2f8937737fe004080d770743609f8896ceb
Shuma Yoshioka <s64.stdio@gmail.com>
2019-01-18 09:12:01 +0900

    Inject keyeventservice

--- backend/app/controllers/MyController.scala
+++ backend/app/controllers/MyController.scala
@@ -2,10 +2,12 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, ControllerComponents}
+import services.MyKeyEventService
 
 @Singleton
 class MyController @Inject()(
-  cc: ControllerComponents
+  cc: ControllerComponents,
+  keyEventService: MyKeyEventService
 ) extends AbstractController(cc) {
 
 }
